37dba12375a0cd89ad5ec58c6f9297003a0a0b2f
chhavi raj singh <chhavirajsingh2000@gmail.com>
2022-07-26 21:32:25 +0530

    Add project files.

---
+++ private_constructor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace private_constructor
+{ 
+    class example
+    {
+        public static int a;
+        private example()
+        {
+            
+        }
+        public static int Add()
+        {
+            return a;
+        }
+
+        public static void getTime()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+     
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            example.getTime();
+            example.a = 201;
+            Console.WriteLine(example.Add());
+            Console.ReadLine();
+        }
+    }
+}
